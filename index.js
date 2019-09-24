4d643101e58220b2b801619896bbf4c5206c3291
Brad Frost <bfrost2893@gmail.com>
2019-09-24 01:13:46 -0400

    Add express server to keep bot goin

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const Telegraf = require('telegraf');
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
